014cfad28e43d108cbc3d6777f72be421b8db842
Justin Dubs <jtdubs@gmail.com>
2013-09-05 16:13:24 -0400

    add more samples and improve sampler diagrams

--- src/App/SamplerDiagrams.hs
+++ src/App/SamplerDiagrams.hs
@@ -6,48 +6,50 @@ import Data.List
 
 import Light.Sampler.Sampler
 
-circleSamples :: (RandomGen g) => Rand g [(Float, Float)]
-circleSamples = sequence $ replicate 900 sampleCircle
-
-squareSamples :: (RandomGen g) => Rand g [(Float, Float)]
-squareSamples = sequence $ replicate 900 sampleSquare
-
-squareStrataSamples :: (RandomGen g) => Rand g [(Float, Float)]
-squareStrataSamples = sampleStrata2D 30 30
-
-circleStrataSamples :: (RandomGen g) => Rand g [(Float, Float)]
-circleStrataSamples = sampleStrataCircle 30 30
-
 main :: IO ()
 main = do
-  putStrLn "graphics_toolkit (\"fltk\");"
+  -- putStrLn "graphics_toolkit (\"fltk\");"
   putStrLn "clf"
-  drawCircleSamples 1 "Circle (Random)" circleSamples
-  drawCircleSamples 2 "Circle (Strata)" circleStrataSamples
-  drawSquareSamples 3 "Square (Random)" squareSamples
-  drawSquareSamples 4 "Square (Strata)" squareStrataSamples
+  drawCircleSamples 3 3 1 "Circle (Center)" $ sampleStrataCentersCircle 30 30
+  drawCircleSamples 3 3 2 "Circle (Strata)" $ sampleStrataCircle 30 30
+  drawCircleSamples 3 3 3 "Circle (Random)" $ sequence $ replicate 900 sampleCircle
+  drawSquareSamples 3 3 4 "Square (Center)" $ sampleStrataCentersSquare 30 30
+  drawSquareSamples 3 3 5 "Square (Strata)" $ sampleStrataSquare 30 30
+  drawSquareSamples 3 3 6 "Square (Random)" $ sequence $ replicate 900 sampleSquare
+  drawPolarSamples  3 3 7 "Polar  (Center)" $ sampleStrataCentersPolar 30 30
+  drawPolarSamples  3 3 8 "Polar  (Strata)" $ sampleStrataPolar 30 30
+  drawPolarSamples  3 3 9 "Polar  (Random)" $ sequence $ replicate 900 samplePolar
 
-drawCircleSamples :: Int -> String -> Rand StdGen [(Float, Float)] -> IO ()
-drawCircleSamples ix title samples = do
-  putStrLn $ "figure (" ++ show ix ++ ");"
+drawCircleSamples :: Int -> Int -> Int -> String -> Rand StdGen [(Float, Float)] -> IO ()
+drawCircleSamples w h ix title samples = do
+  putStrLn $ "subplot (" ++ show w ++ ", " ++ show h ++ ", " ++ show ix ++ ");"
   putStrLn $ "t = linspace(0, 2*pi, 100);"
   putStrLn $ "plot(cos(t), sin(t));"
-  drawSamples title samples
+  drawSamples title (1, 1) samples
 
-drawSquareSamples :: Int -> String ->  Rand StdGen [(Float, Float)] -> IO ()
-drawSquareSamples ix title samples = do
-  putStrLn $ "figure (" ++ show ix ++ ");"
+drawSquareSamples :: Int -> Int -> Int -> String ->  Rand StdGen [(Float, Float)] -> IO ()
+drawSquareSamples w h ix title samples = do
+  putStrLn $ "subplot (" ++ show w ++ ", " ++ show h ++ ", " ++ show ix ++ ");"
   putStrLn $ "rectangle ();"
-  drawSamples title samples
+  drawSamples title (1, 1) samples
 
-drawSamples :: String -> Rand StdGen [(Float, Float)] -> IO ()
-drawSamples title samples = do
+drawPolarSamples :: Int -> Int -> Int -> String ->  Rand StdGen [(Float, Float)] -> IO ()
+drawPolarSamples w h ix title samples = do
+  putStrLn $ "subplot (" ++ show w ++ ", " ++ show h ++ ", " ++ show ix ++ ");"
+  putStrLn $ "rectangle (\"Position\", [0, 0, 2*pi, 1]);"
+  drawSamples title (2*pi, 1) samples
+
+drawSamples :: String -> (Float, Float) -> Rand StdGen [(Float, Float)] -> IO ()
+drawSamples title (w, h) samples = do
+  let fw = w / sqrt ((w*w) + (h*h))
+  let fh = h / sqrt ((w*w) + (h*h))
   s <- evalRandIO samples
   putStrLn   "hold on;"
-  putStrLn   "daspect ([1, 1]);"
-  putStrLn   "pbaspect ([1, 1]);"
+  -- putStrLn $ "daspect ([" ++ show fw ++ ", " ++ show fh ++ "]);"
+  putStrLn $ "pbaspect ([" ++ show fw ++ ", " ++ show fh ++ "]);"
+  putStrLn $ "axis (\"off\", \"nolabel\");"
   putStrLn $ "title (\"" ++ title ++ "\");"
   putStrLn $ "x = [" ++ intercalate ", " (map (show.fst) s) ++ "];"
   putStrLn $ "y = [" ++ intercalate ", " (map (show.snd) s) ++ "];"
-  putStrLn   "scatter(x, y);"
+  putStrLn   "scatter(x, y, 0.1, \"auto\");"
   putStrLn   "hold off;"
--- src/Light/Sampler/Sampler.hs
+++ src/Light/Sampler/Sampler.hs
@@ -1,18 +1,39 @@
 module Light.Sampler.Sampler
-  ( samplePolar, sampleCircle, sampleSquare
-  , sampleStrata1D, sampleStrata2D, sampleStrataCircle, sampleStrataPolar
+  ( sampleLine, samplePolar, sampleCircle, sampleSquare
+  , sampleStrataLine, sampleStrataSquare, sampleStrataCircle, sampleStrataPolar
+  , sampleStrataCentersLine, sampleStrataCentersSquare, sampleStrataCentersCircle, sampleStrataCentersPolar
   )
 where
 
 import Control.Monad.Random
 import Control.Monad
 
-sampleStrata1D :: (RandomGen g) => Int -> Rand g [Float]
-sampleStrata1D n = let w = 1 / fromIntegral n
-                   in mapM (\i -> getRandomR (w * fromIntegral i, w * fromIntegral (i+1))) [0..n-1]
+sampleStrataCentersLine :: (RandomGen g) => Int -> Rand g [Float]
+sampleStrataCentersLine n = let w = 1 / fromIntegral n
+                            in return $ map (\i -> (w * fromIntegral i) + (w / 2)) [0..n-1]
 
-sampleStrata2D :: (RandomGen g) => Int -> Int -> Rand g [(Float, Float)]
-sampleStrata2D n m = sequence [helper (fromIntegral x) (fromIntegral y) | x <- [0..n-1], y <- [0..m-1]]
+sampleStrataCentersSquare :: (RandomGen g) => Int -> Int -> Rand g [(Float, Float)]
+sampleStrataCentersSquare n m = sequence [helper (fromIntegral x) (fromIntegral y) | x <- [0..n-1], y <- [0..m-1]]
+  where w = 1 / fromIntegral n
+        h = 1 / fromIntegral m
+        helper x y = return (w*x + w/2, h*y + h/2)
+
+sampleStrataCentersCircle :: (RandomGen g) => Int -> Int -> Rand g [(Float, Float)]
+sampleStrataCentersCircle n m = do s <- sampleStrataCentersPolar n m
+                                   return $ map (\ (t, r) -> (r * cos t, r * sin t)) s
+
+sampleStrataCentersPolar :: (RandomGen g) => Int -> Int -> Rand g [(Float, Float)]
+sampleStrataCentersPolar n m = sequence [helper (fromIntegral t) (fromIntegral r) | t <- [0..n-1], r <- [0..m-1]]
+  where w = (2 * pi) / fromIntegral n
+        h = 1 / fromIntegral m
+        helper t r = return (w*t + w/2, sqrt (h*r + h/2))
+
+sampleStrataLine :: (RandomGen g) => Int -> Rand g [Float]
+sampleStrataLine n = let w = 1 / fromIntegral n
+                     in mapM (\i -> getRandomR (w * fromIntegral i, w * fromIntegral (i+1))) [0..n-1]
+
+sampleStrataSquare :: (RandomGen g) => Int -> Int -> Rand g [(Float, Float)]
+sampleStrataSquare n m = sequence [helper (fromIntegral x) (fromIntegral y) | x <- [0..n-1], y <- [0..m-1]]
   where w = 1 / fromIntegral n
         h = 1 / fromIntegral m
         helper x y = do px <- getRandomR (w*x, w*(x+1))
@@ -30,6 +51,9 @@ sampleStrataPolar n m = sequence [helper (fromIntegral t) (fromIntegral r) | t <
         helper t r = do pt <- getRandomR (w*t, w*(t+1))
                         pr <- getRandomR (h*r, h*(r+1))
                         return (pt, sqrt pr)
+
+sampleLine :: (RandomGen g) => Rand g Float
+sampleLine = getRandomR (0, 1)
 
 sampleSquare :: (RandomGen g) => Rand g (Float, Float)
 sampleSquare = do
